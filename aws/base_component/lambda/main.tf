a12e1f833fa9ad7420c6b7990f2a9d1f57b8e93b
ajmac3242 <andrew.mccombs@gmail.com>
2026-04-21 21:48:00 -0500

    Update Lambda function resource test comment

--- aws/base_component/lambda/main.tf
+++ aws/base_component/lambda/main.tf
@@ -44,7 +44,7 @@ module "execution_role" {
   tags = var.tags
 }
 
-# Main Lambda function resource test 3
+# Main Lambda function resource test 4
 resource "aws_lambda_function" "this" {
   function_name     = var.function_name
   description   = var.description
